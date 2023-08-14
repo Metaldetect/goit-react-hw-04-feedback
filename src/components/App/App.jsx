03e28aaf5cd1e4d15608c01902365e28eb32e366
Metaldetect <stas.klymenko@icloud.com>
2023-08-14 09:23:25 +0200

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -28,31 +28,29 @@ function App() {
     return total > 0 ? Math.round((good / total) * 100) : 0;
   };
 
-  {
-    return (
-      <>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={['good', 'neutral', 'bad']}
-            onButtonClick={handleButtonClick}
+  return (
+    <>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={['good', 'neutral', 'bad']}
+          onButtonClick={handleButtonClick}
+        />
+      </Section>
+      <Section title="Statistics">
+        {countTotalFeedback() > 0 ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={countTotalFeedback()}
+            positivePercentage={countPositiveFeedbackPercentage()}
           />
-        </Section>
-        <Section title="Statistics">
-          {countTotalFeedback() > 0 ? (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={countTotalFeedback()}
-              positivePercentage={countPositiveFeedbackPercentage()}
-            />
-          ) : (
-            <Notafication message="There is not feedback" />
-          )}
-        </Section>
-      </>
-    );
-  }
+        ) : (
+          <Notafication message="There is not feedback" />
+        )}
+      </Section>
+    </>
+  );
 }
 
 export default App;
